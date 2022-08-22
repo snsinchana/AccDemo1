1cf50f743394c9c99343ce7f47f5ba3a3f2f1460
lab-user <lab-user@code-server-776c6bf99b-cm27j>
2022-08-22 13:19:15 +0000

    Changed java file

--- src/main/java/MercFirstDemo.java
+++ src/main/java/MercFirstDemo.java
@@ -15,7 +15,7 @@ public class MercFirstDemo extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.getWriter().print("MercFirstDemo for Java!\n");
+		resp.getWriter().print("Hi Sinchana\n");
 	}
 
 	public static void main(String[] args) throws Exception {
